75bd6c07d9c0df7777cbb4e9fa63a394be71c931
Jihyeon Seong <tjdwltnsfP1@gmail.com>
2022-03-26 19:47:36 +0900

    Update main.py

--- main.py
+++ main.py
@@ -228,7 +228,7 @@ def main(start_data, end_data):
                 )
         metric_chart = metric_chart.properties(
             height=340,
-            width=200
+            width=300
         ).interactive()
         col2.altair_chart(metric_chart, use_container_width=True)
         
