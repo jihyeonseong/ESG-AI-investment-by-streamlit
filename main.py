efaa9c3d80a0e819474b5a9a443d2e779ad8649a
Jihyeon Seong <tjdwltnsfP1@gmail.com>
2022-03-26 17:49:06 +0900

    Update main.py

--- main.py
+++ main.py
@@ -357,7 +357,7 @@ def main(start_data, end_data):
             color=alt.Color("Confidence:Q", scale=alt.Scale(), legend=None)
         ).properties(
             height=25 * num_neighbors + 100
-        ).configure_axis(grid=False)
+        ).configure_title().configure_axis(grid=False, titleFontSize=20)
         st.altair_chart(conf_plot, use_container_width=True)
 
 
