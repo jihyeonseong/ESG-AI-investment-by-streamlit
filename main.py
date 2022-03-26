c769809cd45641457b6a06e7588c7698f0080d18
Jihyeon Seong <tjdwltnsfP1@gmail.com>
2022-03-26 21:03:52 +0900

    Update main.py

--- main.py
+++ main.py
@@ -165,7 +165,6 @@ def main(start_data, end_data):
         # link_df["URL"] = link_df["URL"].apply(lambda R: f"[{R}]({R})")
         link_df["ARTICLE"] = link_df.URL.apply(get_clickable_name)
         link_df = link_df[["DATE", "ARTICLE"]].to_markdown(index=False)
-        link_df.columns = ["날짜", "기사"]
         URL_Expander.markdown(link_df)
         ####
 
