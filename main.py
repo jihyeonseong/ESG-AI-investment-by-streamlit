1cf8dc9ab05fc1835318265cc6bca21d67b25717
Jihyeon Seong <tjdwltnsfP1@gmail.com>
2022-03-26 23:01:18 +0900

    Update main.py

--- main.py
+++ main.py
@@ -365,7 +365,7 @@ def main(start_data, end_data):
 if __name__ == "__main__":
     args = sys.argv
     if len(args) != 3:
-        start_data = "2022-03-01"
+        start_data = "2021-12-01"
         end_data = "2022-03-25"
     else:
         start_data = args[1]
