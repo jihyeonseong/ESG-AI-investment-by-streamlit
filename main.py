129f347d8f8eace5353e7acfc04c439904bc8b7f
jihyeon seong <tjdwltnsfP1@gmail.com>
2022-04-24 12:46:19 +0900

    Update main.py

--- main.py
+++ main.py
@@ -40,6 +40,9 @@ def load_data(start_data, end_data, flag):
     elif flag == 'UK':
         companies.remove('astrazeneca')
         companies.insert(0,"astrazeneca")
+    elif flag == 'CANADA':
+        comapnies.remove('blackberry ltd')
+        companies.insert(0, 'blackberry ltd')
     elif flag == 'AUSTRALIA':
         companies.remove('national australia bank ltd')
         companies.insert(0, 'national australia bank ltd')
