35a90fa1c2f52732c69084bd4d6dc3b1a3651733
jihyeon seong <tjdwltnsfP1@gmail.com>
2022-04-25 17:58:48 +0900

    Update main.py

--- main.py
+++ main.py
@@ -183,10 +183,10 @@ def main(start_data, end_data):
     
     ####
     URL_Expander.write(f"#### Sample Article Information")
-    link_df = df_company[["DATE", "URL"]].copy().tail(10)
+    link_df = df_company[["DATE", "URL"]].copy().drop_duplicates().tail(5)
     # link_df["URL"] = link_df["URL"].apply(lambda R: f"[{R}]({R})")
     link_df["ARTICLE"] = link_df.URL.apply(get_clickable_name)
-    link_df = link_df[["DATE", "ARTICLE"]].drop_duplicates().tail(5).to_markdown(index=False)
+    link_df = link_df[["DATE", "ARTICLE"]].to_markdown(index=False)
     URL_Expander.markdown(link_df)
     ####   
 
