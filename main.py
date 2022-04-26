5c511b6d10d92776ed18bdc7605f92b629acf5f0
jihyeon seong <tjdwltnsfP1@gmail.com>
2022-04-26 16:53:39 +0900

    Update main.py

--- main.py
+++ main.py
@@ -627,7 +627,7 @@ def main(start_data, end_data):
     pie_chart = alt.Chart(max_sharpe_allocation, title="Maximum Sharpe Ratio Portfolio Allocation").mark_arc(size=30).encode(
                 theta=alt.Theta(field="allocation", type="quantitative"),
                 color=alt.Color(field="company", type="nominal"),
-                ).properties(width=200)
+                )#.properties(width=200)
     st.altair_chart(pie_chart, use_container_width=True) 
     st.write("Annualised Return:", round(rp,2))
     st.write("Annualised Volatility:", round(sdp,2))
