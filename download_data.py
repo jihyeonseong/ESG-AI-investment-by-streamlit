b2995fb5fd7be747fb60e9cb32194436b1d282f4
jihyeon seong <tjdwltnsfP1@gmail.com>
2022-04-29 00:25:42 +0900

    Update download_data.py

--- download_data.py
+++ download_data.py
@@ -5,7 +5,7 @@ import os
 
 class Data:
 
-    def paths(self, data_path):
+    def paths(self, data_path, flag='USA'):
         # Paths to data files
         self.connections = os.path.join(data_path, "connections.csv")
         self.data = os.path.join(data_path, "data_as_csv.csv")
@@ -16,7 +16,8 @@ class Data:
         self.e_score = os.path.join(esg_path, "daily_E_score.csv")
         self.s_score = os.path.join(esg_path, "daily_S_score.csv")
         self.g_score =os.path.join(esg_path, "daily_G_score.csv")
-        self.portfolio = os.path.join(data_path, "portfolio.csv")
+        if flag == 'USA':
+            self.portfolio = os.path.join(data_path, "portfolio.csv")
 
 
 
@@ -28,23 +29,40 @@ class Data:
             raise NameError(f"There isn't data for {dir_name}")
 
         data_path = os.path.join(data_dir, dir_name)
-        self.paths(data_path)
-        data = {"conn": pd.read_csv(self.connections),
-                "data": pd.read_csv(self.data, parse_dates=["DATE"],
-                                 infer_datetime_format=True),
-                "embed": pd.read_csv(self.embeddings),
-                "overall_score": pd.read_csv(self.daily_esg,
-                                  index_col="date", parse_dates=["date"],
-                                 infer_datetime_format=True),
-                "E_score": pd.read_csv(self.e_score, parse_dates=["date"],
-                                 infer_datetime_format=True, index_col="date"),
-                "S_score": pd.read_csv(self.s_score, parse_dates=["date"],
-                                 infer_datetime_format=True, index_col="date"),
-                "G_score": pd.read_csv(self.g_score, parse_dates=["date"],
-                                 infer_datetime_format=True, index_col="date"),
-                "ESG": pd.read_csv(self.avg_esg),
-                "Portfolio": pd.read_csv(self.portfolio),
-                }
+        self.paths(data_path, flag)
+        if flag == 'USA':
+            data = {"conn": pd.read_csv(self.connections),
+                    "data": pd.read_csv(self.data, parse_dates=["DATE"],
+                                     infer_datetime_format=True),
+                    "embed": pd.read_csv(self.embeddings),
+                    "overall_score": pd.read_csv(self.daily_esg,
+                                      index_col="date", parse_dates=["date"],
+                                     infer_datetime_format=True),
+                    "E_score": pd.read_csv(self.e_score, parse_dates=["date"],
+                                     infer_datetime_format=True, index_col="date"),
+                    "S_score": pd.read_csv(self.s_score, parse_dates=["date"],
+                                     infer_datetime_format=True, index_col="date"),
+                    "G_score": pd.read_csv(self.g_score, parse_dates=["date"],
+                                     infer_datetime_format=True, index_col="date"),
+                    "ESG": pd.read_csv(self.avg_esg),
+                    "Portfolio": pd.read_csv(self.portfolio),
+                    }
+        else:
+            data = {"conn": pd.read_csv(self.connections),
+                    "data": pd.read_csv(self.data, parse_dates=["DATE"],
+                                     infer_datetime_format=True),
+                    "embed": pd.read_csv(self.embeddings),
+                    "overall_score": pd.read_csv(self.daily_esg,
+                                      index_col="date", parse_dates=["date"],
+                                     infer_datetime_format=True),
+                    "E_score": pd.read_csv(self.e_score, parse_dates=["date"],
+                                     infer_datetime_format=True, index_col="date"),
+                    "S_score": pd.read_csv(self.s_score, parse_dates=["date"],
+                                     infer_datetime_format=True, index_col="date"),
+                    "G_score": pd.read_csv(self.g_score, parse_dates=["date"],
+                                     infer_datetime_format=True, index_col="date"),
+                    "ESG": pd.read_csv(self.avg_esg),
+                    }
         # Dat column to date (not timestamp)
         data["data"]["DATE"] = data["data"]["DATE"].dt.date
 
